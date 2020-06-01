3710da32ff320acf51b0818a3a95100b48df4e29
maste150hhu <mastremolten2@gmail.com>
2020-06-02 01:50:51 +0200

    add routing for search

--- src/main/java/net/bestofcode/Facebook/controller/FacebookController.java
+++ src/main/java/net/bestofcode/Facebook/controller/FacebookController.java
@@ -3,6 +3,7 @@ package net.bestofcode.Facebook.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class FacebookController {
@@ -10,5 +11,11 @@ public class FacebookController {
     @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/search/{param}")
+    public String search(@PathVariable String param, Model model) {
+        model.addAttribute("searchparam", param);
+        return "search";
     }
 }
