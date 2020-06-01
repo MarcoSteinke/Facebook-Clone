bb2820d1a6c6b2ab5f55812e12ff893004638021
maste150hhu <mastremolten2@gmail.com>
2020-06-02 01:57:34 +0200

    add first search functionality

--- src/main/java/net/bestofcode/Facebook/controller/FacebookController.java
+++ src/main/java/net/bestofcode/Facebook/controller/FacebookController.java
@@ -3,7 +3,7 @@ package net.bestofcode.Facebook.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FacebookController {
@@ -13,8 +13,8 @@ public class FacebookController {
         return "index";
     }
 
-    @GetMapping("/search/{param}")
-    public String search(@PathVariable String param, Model model) {
+    @GetMapping("/search")
+    public String search(@RequestParam String param, Model model) {
         model.addAttribute("searchparam", param);
         return "search";
     }
