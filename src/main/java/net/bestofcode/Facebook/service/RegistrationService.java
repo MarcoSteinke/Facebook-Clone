e418dd42057f57aa6c2b91c3cd419c6963c76211
maste150hhu <mastremolten2@gmail.com>
2020-06-10 00:09:33 +0200

    add RegistrationService

---
+++ src/main/java/net/bestofcode/Facebook/service/RegistrationService.java
@@ -0,0 +1,20 @@
+package net.bestofcode.Facebook.service;
+
+import lombok.AllArgsConstructor;
+import net.bestofcode.Facebook.model.profile.Credential;
+import net.bestofcode.Facebook.persistence.FormData;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    /*public boolean registerUser(FormData formData) {
+        if(formData.isValid()) {
+
+        }
+    }*/
+
+}
