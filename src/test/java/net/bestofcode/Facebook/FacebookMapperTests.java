74db89971a932534eadc46c0a05b7920a4f39865
maste150hhu <mastremolten2@gmail.com>
2020-06-19 02:18:01 +0200

    create tests

---
+++ src/test/java/net/bestofcode/Facebook/FacebookMapperTests.java
@@ -0,0 +1,40 @@
+package net.bestofcode.Facebook;
+
+import net.bestofcode.Facebook.model.User;
+import net.bestofcode.Facebook.persistence.DTO.UserDTO;
+import net.bestofcode.Facebook.persistence.mappers.UserDTOMapper;
+import net.bestofcode.Facebook.service.DatabaseService;
+import net.bestofcode.Facebook.service.RegistrationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+public class FacebookMapperTests {
+
+    @Autowired
+    DatabaseService databaseService;
+    @Autowired
+    RegistrationService registrationService;
+
+    @Test
+    void testMapperInBothDirections() {
+        String initialPassword = "foobar";
+        String initialUsername = "John";
+        String initialEmail = "john@gmail.com";
+        User user = User.create(initialUsername, initialPassword, initialEmail);
+        UserDTOMapper userDTOMapper = new UserDTOMapper();
+
+        UserDTO userDTO = userDTOMapper.mapUserToUserDTO(user);
+        User restoredUser = userDTOMapper.mapUserDTOToUser(userDTO);
+
+        assertEquals("", user.getUsername().getValue()
+                +user.getPassword().getEncryptedPassword()
+                +user.getEmail().getAddress(),
+                restoredUser.getUsername().getValue()
+                        +restoredUser.getPassword().getEncryptedPassword()
+                        +restoredUser.getEmail().getAddress());
+    }
+}
---
+++ src/test/java/net/bestofcode/Facebook/FacebookPersistenceTests.java
@@ -0,0 +1,71 @@
+package net.bestofcode.Facebook;
+
+import net.bestofcode.Facebook.model.User;
+import net.bestofcode.Facebook.model.profile.Password;
+import net.bestofcode.Facebook.model.profile.Username;
+import net.bestofcode.Facebook.persistence.FormData;
+import net.bestofcode.Facebook.service.DatabaseService;
+import net.bestofcode.Facebook.service.RegistrationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+class FacebookPersistenceTests {
+
+	@Autowired
+	DatabaseService databaseService;
+	@Autowired
+	RegistrationService registrationService;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void loadOneUserFromDB() {
+		User user = User.create("Marco", "foobar", "marco@gmail.com");
+		this.databaseService.insertIntoDB(user);
+
+		User loadedUser = this.databaseService.getFromDB(new Username("Marco"));
+
+		assertEquals("", user.getUsername().getValue(), loadedUser.getUsername().getValue());
+
+	}
+
+	@Test
+	void passwordIsAlwaysHashedTheSameWay() {
+		String initialPassword = "foobar";
+		User user = User.create("Marco", initialPassword, "marco@gmail.com");
+		UUID salt = user.getPassword().getSalt();
+
+		Password password = new Password(initialPassword, salt);
+
+		assertEquals("", user.getPassword().getEncryptedPassword(), password.getEncryptedPassword());
+	}
+
+	@Test
+	void passwordIsPersistedAsEncryptedSaltedHash() {
+		String initialPassword = "foobar";
+		String initialName = "Marco";
+		User user = User.create(initialName, initialPassword, "marco@gmail.com");
+		UUID salt = user.getPassword().getSalt();
+		this.databaseService.insertIntoDB(user);
+
+		User receivedUser = this.databaseService.getFromDB(new Username(initialName));
+
+		assertEquals("", receivedUser.getPassword().getEncryptedPassword(), new Password(initialPassword, salt).getEncryptedPassword());
+	}
+
+	@Test
+	void invalidFormDataIsInvalid() {
+		FormData formData = new FormData(null, null, null);
+
+		assertEquals("", formData.isValid(), false);
+	}
+
+}
